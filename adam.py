6cb08b62a7cd874bac10644f5ea37586dddc239f
seominji <55734436+jaaaamj0711@users.noreply.github.com>
2021-04-05 13:48:25 +0900

    Update adam.py

--- adam.py
+++ adam.py
@@ -1,1 +1,22 @@
-print("hello")
+class adam: 
+  def __init__(self):
+    print('생성')
+    
+def sum_two(self, a, b):
+  return a + b
+
+def get_biggest(self, x): 
+  :param x: list of int 
+  :type x: list 
+  :return: The biggest int value 
+  :rtype: int 
+  """ 
+  ret = x[0] 
+  for i in range(1, len(x)): 
+    if x[i] > ret: 
+      ret = x[i] 
+  return ret
+  
+a = adam() 
+print('sum_two() result = ', a.sum_two(1, 3)) 
+print('get_biggest() result = ', a.get_biggest([5,3,4,-10,0]))
