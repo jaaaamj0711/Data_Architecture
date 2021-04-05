046d532e7a61e731bda86dc24f202896f62ac134
Ubuntu <ubuntu@ip-172-31-15-199.us-east-2.compute.internal>
2021-04-05 04:56:44 +0000

    inital commit

--- adam.py
+++ adam.py
@@ -1,21 +1,20 @@
 class adam: 
-  def __init__(self):
-    print('생성')
+    def __init__(self):
+        print('생성')
     
-def sum_two(self, a, b):
-  return a + b
+    def sum_two(self, a, b):
+        return a + b
 
-def get_biggest(self, x): 
-  :param x: list of int 
-  :type x: list 
-  :return: The biggest int value 
-  :rtype: int 
-  """ 
-  ret = x[0] 
-  for i in range(1, len(x)): 
-    if x[i] > ret: 
-      ret = x[i] 
-  return ret
+    def get_biggest(self, x): 
+        :param x: list of int 
+        :type x: list 
+        :return: The biggest int value 
+        :rtype: int 
+        ret = x[0] 
+        for i in range(1, len(x)): 
+            if x[i] > ret: 
+                ret = x[i] 
+        return ret
   
 a = adam() 
 print('sum_two() result = ', a.sum_two(1, 3)) 
